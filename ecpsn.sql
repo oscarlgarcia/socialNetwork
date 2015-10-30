7c5e483d840f7fe5581329826fd66d46e2d02428
ORGC <oscar.garcia@gft.com>
2015-10-30 13:09:49 +0100

    Gamification BD
    BD

--- ecpsn.sql
+++ ecpsn.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 29-10-2015 a las 17:39:52
+-- Tiempo de generación: 30-10-2015 a las 13:08:46
 -- Versión del servidor: 5.6.17
 -- Versión de PHP: 5.5.12
 
